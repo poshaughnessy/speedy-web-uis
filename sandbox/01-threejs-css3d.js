8d107d52c1e78104dff87cb5bb416823263db0ef
Peter O'Shaughnessy <peter.oshaughnessy@pearson.com>
2013-09-09 14:39:28 +0100

    Final tweaks

--- sandbox/01-threejs-css3d.js
+++ sandbox/01-threejs-css3d.js
@@ -23,40 +23,37 @@
     // Camera, position and add to scene
 
     var camera = new THREE.PerspectiveCamera(
-            45,           // Field of View
-            width/height, // Aspect ratio
-            1,            // zNear
-            10000         // zFar
+        45,             // Field of View
+        width / height, // Aspect ratio
+        1,              // zNear
+        1000            // zFar
     );
 
-    camera.position.y = 100;
-    camera.position.z = 800;
-
-    scene.add( camera );
+    camera.position.z = 1000;
 
     // Create a block
 
-    var createBlock = function(x, y, color) {
+    var createBlock = function(x, y, colour) {
 
         var el = document.createElement('div');
         el.className = 'block';
-        el.style.backgroundColor = color;
+        el.style.backgroundColor = colour;
 
         var elTop = document.createElement('div');
         elTop.className = 'side top';
-        el.appendChild( elTop );
-
-        var elLeft = document.createElement('div');
-        elLeft.className = 'side left';
-        el.appendChild( elLeft );
-
-        var elBottom = document.createElement('div');
-        elBottom.className = 'side bottom';
-        el.appendChild( elBottom );
+        el.appendChild(elTop);
 
         var elRight = document.createElement('div');
         elRight.className = 'side right';
-        el.appendChild( elRight );
+        el.appendChild(elRight);
+
+        var elBottom = document.createElement('div');
+        elBottom.className = 'side bottom';
+        el.appendChild(elBottom);
+
+        var elLeft = document.createElement('div');
+        elLeft.className = 'side left';
+        el.appendChild(elLeft);
 
         var obj = new THREE.CSS3DObject( el );
         obj.position.set( x, y, 0 );
@@ -68,34 +65,24 @@
 
     var BLOCK_SIZE = 100;
 
-    createBlock( 0, 0, '#f88d46' );
-    createBlock( 0, BLOCK_SIZE, '#46e1f8' );
-    createBlock( BLOCK_SIZE, BLOCK_SIZE, '#66cc66' );
-    createBlock( 0, BLOCK_SIZE * 2, '#ffcc33' );
+    createBlock(0, 0, 'red');
+    createBlock(0, -BLOCK_SIZE, 'blue');
+    createBlock(BLOCK_SIZE, -BLOCK_SIZE, 'yellow');
+    createBlock(0, -BLOCK_SIZE * 2, 'green');
 
     // Controls
 
     var controls = new THREE.TrackballControls( camera, renderer.domElement );
 
-    // Stats
-
-    var stats = new Stats();
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.bottom = '0px';
-    stats.domElement.style.right = '0px';
-    container.appendChild( stats.domElement );
-
     // Animate
 
     var animate = function() {
 
-        renderer.render( scene, camera );
-
         requestAnimationFrame( animate );
 
-        controls.update();
+        renderer.render( scene, camera );
 
-        stats.update();
+        controls.update();
 
     };
 
